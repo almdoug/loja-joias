6afc5ed38f7edffa2f2bc4beaf23b72e870e8175
almdoug <douglassurf2014@gmail.com>
2024-04-20 12:44:53 -0300

    Implementação da index (Front-end)

--- index.php
+++ index.php
@@ -1,1 +1,182 @@
-Git init
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOMBA</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/all.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-thin.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-solid.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-regular.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/sharp-light.css">
+</head>
+
+<body>
+    <div class="menu">
+        <div class="content">
+            <nav class="left">
+                <ul>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Contatos</a></li>
+                </ul>
+            </nav>
+            <div class="logo">
+                <img src="assets/images/logo.png" alt="" width="100">
+            </div>
+            <nav class="right">
+                <ul>
+                    <li><a href="#">Acesse sua conta</a></li>
+                    <li>Carrinho</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="content">
+        <div class="promo-images">
+            <img src="assets/images/promo.png" alt="" width="1900">
+        </div>
+        <div class="index-products">
+            <div class="category">
+                <ul>
+                    <li><a class="selected" href="#">Anéis</a></li>
+                    <li><a href="#">Aço</a></li>
+                    <li><a href="#">Ouro amarelo</a></li>
+                    <li><a href="#">Ouro branco</a></li>
+                    <li><a href="#">Ouro rosé</a></li>
+                    <li><a href="#">Prata</a></li>
+                </ul>
+                <div class="divider"></div>
+                <ul style="margin-bottom: 50px">
+                    <li><a href="#">Brincos</a></li>
+                    <li><a href="#">Colares</a></li>
+                    <li><a href="#">Pingentes</a></li>
+                    <li><a href="#">Pulseiras</a></li>
+                    <li><a href="#">Jóias infantis</a></li>
+                    <li><a href="#">Jóias masculinas</a></li>
+                    <li><a href="#">Piercins</a></li>
+                    <li><a href="#">Tornozeleiras</a></li>
+                </ul>
+                <a class="btn-primary" href="#">Ver todos</a>
+            </div>
+            <div class="products">
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item1.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Colar Lunari</p>
+                        <span>R$ 600,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item2.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Brincos Eclatique</p>
+                        <span>R$ 900,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item3.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Anel Éclat Étoile0</p>
+                        <span>R$ 375,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item4.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Brincos Serene Sparkles</p>
+                        <span>R$ 500,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item5.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Colar Veletta</p>
+                        <span>R$ 475,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item6.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Colar Zyrra</p>
+                        <span>R$ 550,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item3.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Anel Éclat Étoile0</p>
+                        <span>R$ 375,00</span>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="product-image">
+                        <img src="assets/images/item4.jpg" alt="">
+                    </div>
+                    <div class="item-details">
+                        <p>Brincos Serene Sparkles</p>
+                        <span>R$ 500,00</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="instagram">
+            <h3>Siga nosso Instagram <a href="https://www.instagram.com/joiararaasemijoias/">@joiararaasemijoias</a></h3>
+        </div>
+        <div class="footer">
+            <div class="footer-item">
+                <h3>Fique por dentro</h3>
+                <input type="email" placeholder="E-mail">
+                <a href="#" class="btn-primary" style="width: fit-content;">Assine</a>
+            </div>
+            <div class="footer-item">
+                <h3>Ajuda & Suporte</h3>
+                <ul>
+                    <li><a href="#">Política e privacidade</a></li>
+                    <li><a href="#">Trocas e devoluções</a></li>
+                    <li><a href="#">Termo de uso</a></li>
+                    <li><a href="#">Certificado de segurança</a></li>
+                </ul>
+            </div>
+            <div class="footer-item">
+                <h3>Formas de pagamento</h3>
+                <div class="payments">
+                    <span>Em até 10x sem juros, sem parcela mínima, para peças acima de R$ 100</span>
+                    <div class="payments-icons">
+                        <i class="fa-brands fa-pix"></i>
+                        <i class="fa-solid fa-rectangle-barcode"></i>
+                        <i class="fa-brands fa-cc-visa"></i>
+                        <i class="fa-brands fa-cc-mastercard"></i>
+                        <i class="fa-brands fa-cc-stripe"></i>
+                        <i class="fa-brands fa-cc-paypal"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-item">
+                <h3>Redes sociais</h3>
+                <div style="font-size: 4rem; margin-top: 1.5rem">
+                    <i class="fa-brands fa-square-instagram"></i>
+                    <i class="fa-brands fa-square-facebook"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
